fdb2fdc05796d1c9bf4ee64d4304c5b2f0a3d446
Greg <71923503+Goadney@users.noreply.github.com>
2023-05-12 15:44:19 +0200

    feat : create / update

--- src/Controller/Backend/ArticleController.php
+++ src/Controller/Backend/ArticleController.php
@@ -8,6 +8,7 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/admin/article', name: 'admin_article')]
@@ -34,7 +35,7 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/create', name: '_create', methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
+    public function create(Request $request): Response|RedirectResponse
     // Response est une classe symfo http
     {
         //chaque page a sa propre request donc on peut pas mettre dans le __construct
@@ -58,6 +59,55 @@ class ArticleController extends AbstractController
             return $this->redirectToRoute('admin_article_index');
         }
         return $this->render('Backend/Article/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+
+    #[Route('/update/{id}', name: '_update', methods: ['GET', 'POST'])]
+    public function update(?Article $article, Request $request): Response|RedirectResponse
+    {
+        if (!$article instanceof Article) {
+            $this->addFlash('error', 'Article not found');
+            return $this->redirectToRoute('admin_article_index');
+        }
+
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repo->save($article, true);
+
+            $this->addFlash('success', 'Article updated successfully');
+
+            return $this->redirectToRoute('admin_article_index');
+        }
+
+        return $this->render('Backend/Article/update.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/remove/{id}', name: '_remove', methods: ['GET', 'POST'])]
+    public function remove(?Article $article, Request $request): Response|RedirectResponse
+    {
+        if (!$article instanceof Article) {
+            $this->addFlash('error', 'Article not found');
+            return $this->redirectToRoute('admin_article_index');
+        }
+
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repo->remove($article, true);
+
+            $this->addFlash('success', 'Article removed successfully');
+
+            return $this->redirectToRoute('admin_article_index');
+        }
+
+        return $this->render('Backend/Article/update.html.twig', [
             'form' => $form
         ]);
     }
