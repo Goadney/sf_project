f0d4f02e89bb94e3f60ab59fda46afeca73881d5
Greg <71923503+Goadney@users.noreply.github.com>
2023-05-16 16:52:50 +0200

    feat : upload img for article , scss card

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ArticlesType extends AbstractType
 {
@@ -23,6 +24,21 @@ class ArticlesType extends AbstractType
                     'placeholder' => 'La dernière maj d\'Apple !'
                 ]
             ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image :',
+                'required' => false,
+                'delete_label' => "Supprimer l\'image",
+                'allow_delete' => true,
+                'download_uri' => false, //interdire le dl de l'image via le form
+                'image_uri' => true,
+                'attr' => [
+                    'class' => 'form-control-file'
+                ]
+            ])
+
+
+
+
             ->add('contenu', TextareaType::class, [
                 'label' => 'Contenu :',
                 'required' => true,
