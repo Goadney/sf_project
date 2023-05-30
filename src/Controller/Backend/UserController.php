97e9c57b70c379f7cb8034a3a2d9f39653b707e8
Greg <71923503+Goadney@users.noreply.github.com>
2023-05-30 17:00:11 +0200

    feat : mail reset password

--- src/Controller/Backend/UserController.php
+++ src/Controller/Backend/UserController.php
@@ -18,7 +18,7 @@ class UserController extends AbstractController
         private readonly UserRepository $repo
     ) {
     }
-    #[Route('gestion', name: '_gestion')]
+    #[Route('/gestion', name: '_gestion')]
     public function gestion(): Response
     {
         return $this->render('Backend/User/gestion.html.twig', [
@@ -47,5 +47,24 @@ class UserController extends AbstractController
         return $this->render('Backend/User/update.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/delete/{id}', name: '_delete', methods: ['DELETE', 'POST'])]
+    public function remove(?User $user, Request $request): RedirectResponse
+    //?Article $article permet avec l'id dans le route de selectionner l'article avec cette id si existant "?"
+    // car si il n'existe pas ce ne sera pas du type objet Article
+    {
+        if (!$user instanceof User) {
+            $this->addFlash('error', 'User not found');
+            return $this->redirectToRoute('admin_user_gestion');
+        }
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('token'))) {
+            $this->repo->remove($user, true);
+
+            $this->addFlash('success', 'User deleted successfully');
+            return $this->redirectToRoute('admin_user_gestion');
+        }
+        $this->addFlash('error', 'token invalid');
+        return $this->redirectToRoute('admin_user_gestion');
     }
 }
